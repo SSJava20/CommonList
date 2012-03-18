358177a4b2811d3626af2535c44c36659f460dab
Vladislav <root@stvad.org>
2012-03-18 21:47:47 +0200

    all added

---
+++ src/main/java/list/Iterator.java
@@ -0,0 +1,20 @@
+package list;
+
+public class Iterator {
+	private ListInt list;
+	private int i = 0;
+
+	public Iterator(ListInt list) {
+		this.list = list;
+	}
+
+	public boolean hasNext() {
+		if (i < list.size())
+			return true;
+		return false;
+	}
+
+	public int next() {
+		return list.get(i++);
+	}
+}
---
+++ src/main/java/list/ListInt.java
@@ -0,0 +1,35 @@
+package list;
+
+public abstract class ListInt {
+
+	protected int size;
+
+	protected ListInt() {
+	}
+
+	public abstract void add(int index, int item);
+
+	public abstract void addFirst(int item);
+
+	public abstract void addLast(int item);
+
+	public abstract void clear();
+
+	public abstract void del(int index);
+
+	public abstract int get(int index);
+
+	public abstract void set(int index, int item);
+	
+	public Iterator iterator() {
+		return new Iterator(this);
+	} 
+
+	public int size() {
+		return size;
+	}
+
+	public void sort() {
+		
+	}
+}
