3e7039d3300d59b6a976ab2e67f0a1dc6e576b8b
Vladislav <root@stvad.org>
2012-03-19 21:01:57 +0200

    Linked

---
+++ src/main/java/list/LinkedListInt.java
@@ -0,0 +1,53 @@
+package list;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stvad
+ * Date: 19.03.12
+ * Time: 21:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class LinkedListInt extends ListInt
+{
+    @Override
+    public void add(int index, int item)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void addFirst(int item)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void addLast(int item)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void clear()
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void del(int index)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int get(int index)
+    {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void set(int index, int item)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
