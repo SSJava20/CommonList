d94d081cad57e0a64d3199414ead56941e3bc095
Vladislav <root@stvad.org>
2012-03-21 00:48:57 +0200

    непашет, но я спать..

---
+++ src/main/java/list/DoubleLinkedListInt.java
@@ -0,0 +1,129 @@
+package list;
+
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stvad
+ * Date: 21.03.12
+ * Time: 0:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class DoubleLinkedListInt extends ListInt
+{
+    protected class DoubleListNode
+    {
+        DoubleListNode next;
+        DoubleListNode previous;
+        //        DoubleListNode begin;
+        int data;
+
+        public DoubleListNode(DoubleListNode prev, DoubleListNode nxt, int ndata)
+        {
+            next = nxt;
+            data = ndata;
+            previous = prev;
+        }
+    }
+
+    protected DoubleListNode begin;
+    protected DoubleListNode end;
+
+    public DoubleLinkedListInt()
+    {
+        clear();
+    }
+
+    protected DoubleListNode getNode(int index)
+    {
+        DoubleListNode temp = begin;
+        int cnt = 0;
+        while (cnt++ != index && temp != null)
+            temp = temp.next;
+
+        return temp;
+    }
+
+    @Override
+    public void add(int index, int item)
+    {
+        if (index == 0 || end == null)
+            addFirst(item);
+        else
+        {
+            DoubleListNode addAfter = getNode(index);
+            DoubleListNode added = new DoubleListNode(addAfter, addAfter.next, item);
+            addAfter.next = added;
+            size++;
+        }
+
+    }
+
+    @Override
+    public void addFirst(int item)
+    {
+        DoubleListNode added = new DoubleListNode(null, begin, item);
+        begin = added;
+        if (size == 0)
+            end = added;
+        size++;
+    }
+
+    @Override
+    public void addLast(int item)
+    {
+        if (end == null)
+            addFirst(item);
+        else
+        {
+            DoubleListNode added = new DoubleListNode(end, null, item);
+            end = added;
+            size++;
+        }
+    }
+
+    @Override
+    public void clear()
+    {
+        begin = null;
+        end = null;
+        size = 0;
+    }
+
+    @Override
+    public void del(int index)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int get(int index)
+    {
+        return getNode(index).data;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void set(int index, int item)
+    {
+        getNode(index).data = item;
+    }
+
+    @Override
+    public int indexOf(int item)
+    {
+        int cnt = 0;
+        DoubleListNode tmp = begin;
+        while (begin.data != item && tmp != null)
+        {
+            tmp = tmp.next;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    @Override
+    public Iterator<Integer> getInnerIterator()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
